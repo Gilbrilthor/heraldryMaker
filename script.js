3661fa8d861aca56fa8a523716224c6d63b56592
Matthew Paul <mpaul0416@gmail.com>
2018-01-09 21:16:18 -0500

    add tinctures to JSON and script. Refactor function to get random member of array.

--- script.js
+++ script.js
@@ -16,14 +16,26 @@ function hideLoad() {
 	message.style.visibility = 'hidden';
 }
 
+function getRandom(array) {
+	var randomIndex = Math.floor(Math.random() * heraldry.divisions.length);
+	var randomMember = array[randomIndex];
+
+	return randomMember;
+}
+
 function postLoad(heraldry) {
 	var field = document.getElementById('field');
+	var tincture = document.getElementById('tincture');
+	var charge = document.getElementById('charge');
 
-	var randomIndex = Math.floor(Math.random() * heraldry.divisions.length);
-	var randomField = heraldry.divisions[randomIndex];
+	var randomField = getRandom(heraldry.divisions);
 	console.log('random field -> "' + randomField + '"');
 
+	var randomTincture = getRandom(heraldry.tinctures);
+	console.log('random tincture -> "' + randomField + '"');
+
 	field.textContent = randomField;
+	tincture.textContent = randomTincture;
 
 	console.log('postLoad finished');
 }
